eded1c288713e664580cd7cba3552359d0fe4350
Lord-Dralkhy <lorddralkhy@outlook.com>
2023-12-21 17:47:02 -0300

    Commit Inicial

---
+++ Conversor PDF2DOCX/conversorPDF2DOCX.py
@@ -0,0 +1,35 @@
+#V 1.0.0
+
+import tkinter as tk
+
+from pdf2docx import Converter
+from tkinter import simpledialog, messagebox
+
+# Cria uma janela principal oculta
+root = tk.Tk()
+root.withdraw()
+
+# Solicita um texto ao usuário usando a caixa de diálogo
+PDFSelecionado = simpledialog.askstring("Escolher PDF", "Digite o caminho do PDF, com o nome do arquivo, e sem o formato:")
+
+if PDFSelecionado == "Criado por":
+
+    # Exibe uma mensagem na tela com um botão "OK"
+    messagebox.showinfo("Criado por:", "Este programa foi feito por:\nLord Dralkhy - Magno da Silva Gomes")
+
+else:
+
+    # Salvar o diretório dos arquivos a ser trabalhados
+    arquivoPDF = PDFSelecionado + '.pdf'
+    arquivoDOCX = PDFSelecionado + '.docx'
+
+    # Selecionar o arquivo PDF
+    conv = Converter(arquivoPDF)
+
+    # Converter para docx
+    conv.convert(arquivoDOCX)
+
+    # Encerrar o processo
+    conv.close()
+
+print('Feito')
---
+++ README.md
@@ -0,0 +1,10 @@
+# Módulos do UiPath - Tradutor de PDF
+
+Um conjunto de executáveis que servem para auxiliar o robô tradutor. Todos escritos em Python, e de código aberto.
+
+Aviso:
+Provavelmente o antvírus irá detectar os executáveis como vírus, basta adicionar a excessão.
+
+# Conversor de PDF para DOCX
+
+Este módulo serve para converter um arquivo .pdf para um .docx . Ele Irá auxiliar o robô, tornando os PDFs editáveis.
